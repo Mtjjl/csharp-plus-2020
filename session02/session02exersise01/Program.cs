7d5418e4bd50199a30fdd888908195124798126b
LAPTOP-48TDI2LP\miral <mirali.mtjjl@gmail.com>
2020-08-26 09:47:35 +0200

    enisial comite

---
+++ session02/session02exersise01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace session02exersise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
