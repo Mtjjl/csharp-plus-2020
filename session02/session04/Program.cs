50c14cc649ee21c7f90489352b1f8082a0ae56a1
LAPTOP-48TDI2LP\miral <mirali.mtjjl@gmail.com>
2020-08-28 13:09:05 +0200

    vorkt in number

--- session02/session04/Program.cs
+++ session02/session04/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
 
 namespace session04
 {
@@ -6,14 +9,75 @@ namespace session04
     {
         static void Main(string[] args)
         {
+            int Q = 0;
             Console.WriteLine("Ange ett antal siffror, separerat med kommatecken");
             var input = Console.ReadLine();
             var inputAray = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            double[] numberArray = new double[inputAray.Length];
+            
+            for (int i=0; i<inputAray.Length;i++)
+            {
+                try
+                {
+                    numberArray[i] = Convert.ToDouble(inputAray[i]);
+
+                }
+
+                catch(Exception)
+                {
+                    numberArray[i] = 0;
+                }
+
+                finally //körs alltid,
+                {
+
+                }
+
+
+                for (int i = 0; i < inputAray.Length; i++)
+                {
+                    bool parsed = double.TryParse(inputAray[i], CultureInfo.InvariantCulture, out double paredValue);
+
+                  if(parsed==true)
+                    {
+                        numberArray[i] = paredValue;
+                    }
+
+                    else 
+                    {
+                        numberArray[i] = null;
+                    }
+                }
+
+
+            }
+            /*
+
+            int execeptionStatus=1
+
+                try
+            {
+                execeptionStatus
+            }
+            
+             throw new exeption ("provocerat fel"); 
+             */
 
             foreach (var number in inputAray)
             {
                 Console.WriteLine("Värdet är " + number);
+                Console.WriteLine("");
+                
             }
+          
+
+
+
+
+            
+
+            Console.WriteLine(" l" +inputAray.Length);
+
         }
     }
 }
