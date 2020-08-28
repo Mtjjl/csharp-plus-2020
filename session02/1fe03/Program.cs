5bb9a988908141a2e5e8cfc9890f3063d047878f
LAPTOP-48TDI2LP\miral <mirali.mtjjl@gmail.com>
2020-08-28 09:26:31 +0200

    aray test

---
+++ session02/1fe03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1fe03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you whant to lel me yore name? ");
+            var key = Console.
+           
+            if()
+            {
+                
+            }
+
+            else
+            {
+             
+            }
+
+string namn= Console.ReadLine();
+            Console.WriteLine("hello"+namn);
+            Console.ReadKey();
+           
+
+                
+        }
+    }
+}
---
+++ session02/session04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace session04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange ett antal siffror, separerat med kommatecken");
+            var input = Console.ReadLine();
+            var inputAray = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var number in inputAray)
+            {
+                Console.WriteLine("Värdet är " + number);
+            }
+        }
+    }
+}
